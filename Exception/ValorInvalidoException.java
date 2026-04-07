2fff98ec6b93f7e1e5f49223879d6a3e0a082db8
Rafael Wesley <rafaelwesleydasilvafelizardo@gmail.com>
2026-04-06 23:05:39 -0300

    Delete Exception directory

--- Exception/ValorInvalidoException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class ValorInvalidoException extends RuntimeException {
-
-    public ValorInvalidoException(double valor) {
-        super(String.format("Valor inválido: R$ %.2f. O valor deve ser maior que zero.", valor));
-    }
-}
