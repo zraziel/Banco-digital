e355158200a368b2ea30f4e80ea13f68d15bbdc0
Rafael Wesley <rafaelwesleydasilvafelizardo@gmail.com>
2026-04-06 21:17:19 -0300

    Add files via upload
    Interfaces

--- Interface/Tributavel.java
+++ Interface/Tributavel.java
@@ -1,1 +1,9 @@
+package interfaces;
 
+public interface Tributavel {
+
+    double TAXA_CORRENTE = 0.01;  // 1% sobre saques
+    double TAXA_POUPANCA = 0.005; // 0.5% sobre rendimentos
+
+    double calcularTaxa();
+}
